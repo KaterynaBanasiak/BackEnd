90accd3d5413f6b7f2dd66c10b444a874b2aab32
KaterynaBanasiak <katerynabanasiak1@gmail.com>
2025-05-03 17:39:27 +0200

    Добавлены данные 9 урока

--- lesson9/demo20250429_1/src/main/java/de/ait/Main.java
+++ lesson9/demo20250429_1/src/main/java/de/ait/Main.java
@@ -2,7 +2,7 @@ package de.ait;
 
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person("Jack", 10, 30, 105);
+        Person p = new Person("Jack", 10,30, 105);
         int w= p.getWeight();
         System.out.println(p);
         Person mike = Person.builder()
